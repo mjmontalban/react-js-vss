38e3a0effb01fbee028336b68edf04e1cdbaadef
mjmontalban <mj3@viptutorsco@gmail.com>
2025-04-23 04:42:31 +0800

    crud

--- src/components/dialogs/edit-product.tsx
+++ src/components/dialogs/edit-product.tsx
@@ -11,7 +11,7 @@ import {
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 interface IProducts {
     id: number,
